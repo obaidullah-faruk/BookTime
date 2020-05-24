e38c651e47763f67d157fa93477de595cb2f7124
obaidullah.faruk <obaidullah.faruk05@gmail.com>
2020-05-24 12:25:55 +0600

    before setup

--- main/models.py
+++ main/models.py
@@ -1,3 +1,117 @@
 from django.db import models
+from django.contrib.auth.models import AbstructUser, BaseUserManager
 
-# Create your models here.
+
+class ActiveManager(models.Manager):
+    def active(self):
+        return self.filter(active=True)
+
+
+class ProductTagManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
+
+class ProductTag(models.Model):
+    # products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    objects = ProductTagManager()
+    
+    def __str__(self):
+        return self.name
+
+    def natural_key(self):
+        return (self.slug,)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField(blank=True)
+    tags = models.ManyToManyField(ProductTag, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2) 
+    # Decimal place: The position of a digit to the right of a decimal point.
+    slug = models.SlugField(max_length=48)
+    active = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    objects = ActiveManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE
+    )
+    image = models.ImageField(
+        upload_to="product-images"
+        )
+    thumbnail = models.ImageField(
+        upload_to="product-thumbnails", null=True
+    )
+
+    def __str__(self):
+        return self.product
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(
+                "Superuser must have is_staff=True."
+            )
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(
+                "Superuser must have is_superuser=True."
+            )
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    @property
+    def is_employee(self):
+        return self.is_active and (
+            self.is_superuser
+            or self.is_staff
+            and self.groups.filter(name="Employees").exists()
+        )
+
+    @property
+    def is_dispatcher(self):
+        return self.is_active and (
+            self.is_superuser
+            or self.is_staff
+            and self.groups.filter(name="Dispatchers").exists()
+        )
--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from main import views
+
 
 urlpatterns = [
+     path("",
+         TemplateView.as_view(template_name="home.html"),
+         name="home",),
     path("about-us/",
          TemplateView.as_view(template_name="about_us.html"),
          name="about_us",),
-    path("",
-         TemplateView.as_view(template_name="home.html"),
-         name="home",),
+     path("contact-us/",
+          views.ContactUsView.as_view(),
+          name="contact_us"),
+     path("products/<slug:tag>/",
+          views.ProductListView.as_view(),
+          name="products",),
+     path(
+        "product/<slug:slug>/",
+        DetailView.as_view(model=models.Product),
+        name="product",
+    ),
 ]
